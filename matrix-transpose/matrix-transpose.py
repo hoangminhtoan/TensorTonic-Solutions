840ad9318275ed6452533c4f649e7af821e260cb
Toan Minh Hoang <hoangminhtoant2l1@yahoo.com>
2026-03-02 22:41:12 +0700

    Update solution for matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -5,5 +5,4 @@ def matrix_transpose(A):
     Return the transpose of matrix A (swap rows and columns).
     """
     # Write code here
-    A = np.array(A)
-    return A.T
+    return np.array([[A[i][j] for i in range(len(A))] for j in range(len(A[0]))])
